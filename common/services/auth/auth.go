68c0ae39620b838ca0260f175d8c3b39daec5fa9
Dennis Dang <dangggdennis@gmail.com>
2020-10-05 14:35:33 -0400

    add alex as admin

--- common/services/auth/auth.go
+++ common/services/auth/auth.go
@@ -2,7 +2,7 @@ package auth
 
 // IsAdmin identifies admin status
 func IsAdmin(discordID string) bool {
-	admins := []string{"192906671167635457"}
+	admins := []string{"192906671167635457", "691353925093294161"}
 
 	for _, id := range admins {
 		if id == discordID {
